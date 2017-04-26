b1aebcc26d50ad775e67dba17a3ef54170ffab74
Emil Snorre Alnæs <esa@redpill-linpro.com>
2017-04-26 11:21:58 +0200

    now with env

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
+
+
 application = Flask(__name__)
+
 
 @application.route("/")
 def hello():
-    return "Ayyyy 😎"
+    return os.getenv('AYY', "Ayyyy 😎")
+
 
 if __name__ == "__main__":
     application.run()
